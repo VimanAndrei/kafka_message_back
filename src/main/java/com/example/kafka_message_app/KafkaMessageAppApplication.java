2b86c3a526d78863cddca12021abebb3f7468367
Viman Andrei <andrei.viman04@gmail.com>
2023-05-09 11:07:22 +0300

    initial commit

---
+++ src/main/java/com/example/kafka_message_app/KafkaMessageAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.kafka_message_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaMessageAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KafkaMessageAppApplication.class, args);
+    }
+
+}
